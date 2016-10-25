e29f7f714e863d70c32c309b0ce54fc714cc7925
RomainChassere <romainchassere@gmail.com>
2016-10-26 01:20:01 +0200

    Page Vote + début anims
    Vote quasi finie, manque qq anims, need finir pas d'accueil de facon
    propre et ajouter anims + Timer fonctionnel + Manque images sur index

---
+++ js/script.js
@@ -0,0 +1,34 @@
+$( document ).ready(function() {
+  var block = $('nav');
+  var navPosition = block.offset().top;
+  var fixed = false;
+
+  $(document).on('scroll', function() {
+    if($(window).scrollTop() > navPosition) {
+      fixed = true;
+      if (fixed == true){
+        block.addClass('fixed');
+        $('#panier').hide(500);
+        console.log('class');
+        fixed = false;
+      }
+    } else {
+      block.removeClass('fixed');
+      $('#panier').show(400);
+      console.log('class');
+    }
+  });
+
+  $('#like').on("mouseover", function() {
+    console.log('OUI');
+    $('#like').addClass('animated pulse');
+    $('#pouce').addClass('white');
+    $('#pouce').addClass('blue'); 
+  });
+
+  $('#fa-upload').on("mouseover", function() {
+    console.log('OUI200');
+    $('#fa-upload').addClass('animated bounce');
+  });
+
+  });
